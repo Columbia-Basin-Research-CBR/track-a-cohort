6c55ba032b1ffac28a579364547f46150ab14466
caitobrien <120421572+caitobrien@users.noreply.github.com>
2024-07-08 15:52:27 -0700

    ammend to last commit

--- R/load_dependencies.R
+++ R/load_dependencies.R
@@ -14,3 +14,4 @@ library(scales)
 library(tidyverse) #if in package form pull out specific libraries -- dplyr, ggplot2, tidyr, lubridate, etc
 library(usethis) 
 library(xts)
+library(zoo)
---
+++ apps.R/utils_SacPAStheme.R
@@ -0,0 +1,46 @@
+#' SacPAStheme
+#'
+#' @description A utils function setting adjusting the default shinydashboard theme
+#'
+#' @return The return value, will reflect the CBR website color theme
+#'
+#' @noRd
+
+SacPAStheme<- fresh::create_theme(
+  fresh::adminlte_color(
+    light_blue = "#bcb18b", #status = "primary"
+    aqua = "#e0d6b0", #status = "info"
+    green = "#c3d7e2", #status = "success"
+    teal = "#c3d7e2",
+    gray_lte = "#e0d6b0",
+    blue = "#bcb18b"
+  ),
+  fresh::adminlte_sidebar(
+    width = "300px",
+    # dark_bg = "#F8F8F8",
+    # dark_color = "black",
+    # dark_hover_bg = "#c3d7e2", #none of the colors seem to do anything other than bg//switch to css option for more piecemeal customization
+    # dark_hover_color = "#e0d6b0",
+    # dark_submenu_bg = "#F8F8F8",
+    # dark_submenu_color = "black",
+    # dark_submenu_hover_color = "#c3d7e2",
+    light_bg = "#e0d6b0",
+    light_hover_bg = "#c3d7e2",
+    light_color = "black",
+    light_hover_color = "#e0d6b0",
+    light_submenu_bg = "#F8F8F8",
+    light_submenu_color = "black",
+    light_submenu_hover_color = "#c3d7e2"
+  ),
+  fresh::adminlte_global(
+    content_bg = "#FFF",
+    box_bg = "#F8F8F8",
+    info_box_bg = "#c3d7e2"
+  )
+)
+
+#c3d7e2
+#e0d6b0
+#bcb18b
+#04224e
+
