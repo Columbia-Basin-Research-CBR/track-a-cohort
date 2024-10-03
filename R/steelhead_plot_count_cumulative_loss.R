1c6a3f8ad84e97ed781e941577180ba02469e9ef
caitobrien <csobrien@uw.edu>
2024-10-03 10:27:34 -0700

    update stl cumloss plot for no new data reported in WY

--- R/steelhead_plot_count_cumulative_loss.R
+++ R/steelhead_plot_count_cumulative_loss.R
@@ -8,77 +8,81 @@ require(here)
 require(ggh4x)
 
 
-# load data
-
+# Load data
 load(here("data/steelhead_loss_data.rda"))
-# load wDay to month function
+# Load wDay to month function
 source(here("R/utils_fct_wday_to_month.R"))
 
-
-#wrangle data
-
-#set current year
+# Set current year
 source(here("R/utils_fct_assign_current_water_year.R"))
 current_year <- assign_current_water_year()
+previous_year <- current_year - 1
 
 # Get the current timestamp
 timestamp <- format(Sys.time(), "%d %b %Y %H:%M:%S %Z")
 
-#extract max cumloss of 2024 to adjust color 
-max2024LAD <- steelhead_loss_data %>% 
-  group_by(WY) %>% 
-  filter(WY == current_year) %>% 
+# Check if there is data for the current water year
+current_year_data <- steelhead_loss_data %>% filter(WY == current_year)
+use_previous_year <- nrow(current_year_data) == 0
+
+# If no data for the current year, use the previous year
+if (use_previous_year) {
+  current_year_data <- steelhead_loss_data %>% filter(WY == previous_year)
+  current_year_label <- paste0("WY", previous_year, " cumulative loss")
+  caption_note <- paste0("No reported data for current WY: ", current_year)
+} else {
+  current_year_label <- paste0("WY", current_year, " cumulative loss")
+  caption_note <- ""
+}
+
+# Extract max cumloss of the current year to adjust color
+maxLAD <- current_year_data %>% 
   summarise(max_cumloss = max(cumloss)) %>% 
   pull(max_cumloss)
 
-#extract maximum cumloss for each year (for labels)
+# Extract maximum cumloss for each year (for labels)
 max_cumloss_per_year <- steelhead_loss_data %>%
-  filter(!is.na(hydro_type_grp)) %>% #currently removes WY2023 or year prior to current year since no hydro assigned
+  filter(!is.na(hydro_type_grp)) %>% # Currently removes WY2023 or year prior to current year since no hydro assigned
   group_by(WY) %>%
   filter(cumloss == max(cumloss)) %>% 
   distinct()
 
-#appending current water year results for each facet regardless of year
-loss_current_year <- steelhead_loss_data %>% filter(WY == current_year) %>% select(wDay, cumloss)
+# Appending current water year results for each facet regardless of year
+loss_current_year <- current_year_data %>% select(wDay, cumloss)
 
-# plot
-p<- steelhead_loss_data %>%
+# Plot
+p <- steelhead_loss_data %>%
   group_by(WY, wDay, status, hydro_type_grp) %>% 
-  # filter(cumloss < 10000) %>%
-  #currently removes WY2023 or year prior to current year since no hydro assigned - consider alternative
   filter(!is.na(hydro_type_grp)) %>% 
   ggplot() +
-  geom_line( data = . %>% filter(WY == current_year ), aes(x = wDay, y = cumloss, group = WY), color = "black") +
-  geom_line( data = . %>% filter(WY != current_year ), aes(x = wDay, y = cumloss, group = WY, color =  hydro_type_grp)) +
-  geom_hline(yintercept = max2024LAD, linetype = "dashed", color = "black") +
-  geom_text(aes(x = 330, y = max2024LAD), label = "WY2024 cumulative loss", size = 2.5, vjust = -0.5, fontface = "plain" ) +
+  geom_line(data = . %>% filter(WY == current_year), aes(x = wDay, y = cumloss, group = WY), color = "black") +
+  geom_line(data = . %>% filter(WY != current_year), aes(x = wDay, y = cumloss, group = WY, color = hydro_type_grp)) +
+  geom_hline(yintercept = maxLAD, linetype = "dashed", color = "black") +
+  geom_text(aes(x = 330, y = maxLAD), label = current_year_label, size = 2.5, vjust = -0.5, fontface = "plain") +
   gghighlight::gghighlight() +
-  ggrepel::geom_text_repel(data = subset(max_cumloss_per_year, cumloss >= max2024LAD ), #& WY !=2001
+  ggrepel::geom_text_repel(data = subset(max_cumloss_per_year, cumloss >= maxLAD), 
                            aes(x = wDay, y = cumloss, label = WY), 
-                           size = 3, direction = "y",vjust = 1,  nudge_y = 1, min.segment.length = 0, force = 15) +
-  # geom_text(data = year2001_data, 
-  #           aes(x = 170, y = 9500, label = "*2001\nmax value = 20,062"), 
-  #           size = 3, fontface = "plain") +
+                           size = 3, direction = "y", vjust = 1, nudge_y = 1, min.segment.length = 0, force = 15) +
   labs(x = 'Date', 
        y = 'Cumulative Loss', 
        title = 'Cumulative Loss by BiOp Status and Hydrologic Classification Index',
-       subtitle = paste0("Species: Clipped and Unclipped Steelhead\nData Years: WY",min(steelhead_loss_data$WY), " to WY", max(steelhead_loss_data$WY),
-                         "\nCurrent Cumulative Loss: ", round(max2024LAD,2)),
-       caption = paste0("Data source: Preliminary data from CDFW; subject to revision. Hydrological Classification Index from CDEC\n", timestamp)) +
-  scale_x_continuous(breaks = seq(1, 365, by = 61), labels = wDay_to_month( seq(1, 365, by = 61))) + 
-  scale_y_continuous(labels = scales::comma,limits = c(0, 40000),  expand = c(0, 0)) +
+       subtitle = paste0("Species: Clipped and Unclipped Steelhead\nData Years: WY", min(steelhead_loss_data$WY), " to WY", max(steelhead_loss_data$WY),
+                         "\nCurrent Cumulative Loss: ", round(maxLAD, 2)),
+       caption = paste0(caption_note, "\nData source: Preliminary data from CDFW; subject to revision. Hydrological Classification Index from CDEC\n", timestamp, "\n")) +
+  scale_x_continuous(breaks = seq(1, 365, by = 61), labels = wDay_to_month(seq(1, 365, by = 61))) + 
+  scale_y_continuous(labels = scales::comma, limits = c(0, 40000), expand = c(0, 0)) +
   scale_color_manual(values = c("#D95F02", "#00BFFF")) +
-  ggh4x::facet_nested(hydro_type_grp ~ status ) + 
-  geom_line(data = loss_current_year, aes(x = wDay, y = cumloss), color = "black",size = 1) +
+  ggh4x::facet_nested(hydro_type_grp ~ status) + 
+  geom_line(data = loss_current_year, aes(x = wDay, y = cumloss), color = "black", size = 1) +
   theme_minimal() +
   theme(
-    # axis.line = element_line(color = "grey"),
     panel.grid.major = element_line(linetype = "dotted"),
     panel.border = element_rect(color = "grey", fill = NA),
     panel.spacing = unit(.5, "cm"),
     axis.ticks = element_line(size = 0.5),
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank(),
-    text = element_text(size = 15))
+    text = element_text(size = 15)
+  )
 
 print(p)
